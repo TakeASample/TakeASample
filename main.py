7019460dd6a4dcd2ebfbccf0556ba9b4caa1a127
Vachanmn123 <vachanmn123@gmail.com>
2021-07-06 12:50:25 +0530

    added about, changed jinja logic

--- main.py
+++ main.py
@@ -5,27 +5,29 @@ import os
 app = Flask(__name__)
 Misaka(app)
 
-def get_mds():
-    section = 'helloworld'
+def get_mds(section_name):
+    section = 'python/basics/helloworld'
     cheatsheets = []
-    for file in os.listdir('./cheatsheets'):
-        cheatsheets.append(str(file).replace('.md',''))
+    for md in os.listdir(f'./cheatsheets/{section_name}'):
+        cheatsheets.append(str(md).replace('.md', ''))
     return sorted(cheatsheets)
+
+app.jinja_env.globals.update(get_mds = get_mds)
 
 @app.route('/')
 def hello():
-    return render_template('index.html', mds=get_mds())
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return render_template('about.html', mds=get_mds())
+    return render_template('about.html')
 
 @app.route('/cheat')
 def cheat():
     global section
     with open(f'./cheatsheets/{section}.md', 'r') as f:
         data = f.read()
-    return render_template('cheatsheet.html', data=data, mds=get_mds())
+    return render_template('cheatsheet.html', data=data)
 
 @app.route('/b')
 def b():
